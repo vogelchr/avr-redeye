ffc0c3e23b19f9b248f2b03c7dd56a05451fcf19
Christian Vogel <vogelchr@vogel.cx>
2011-12-10 16:36:34 +0100

    main: Removed debugging, added hello message.

--- serial2redeye.c
+++ serial2redeye.c
@@ -1,45 +1,38 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 
 #include "avr-redeye.h"
 #include "serial_util.h"
 
-void
+const char hello[] PROGMEM =\
+	"This is serial2redeye.\r\n"
+	"It sends received data to a HP82240A via Infrared.\r\n"
+	"It doesn't care about flow control, be sure not to overflow\r\n"
+	"either the AVR or the HP82240A buffer.";
+
+int
 main(){
+	int i;
+
 	/* -------------- serial port --------------------- */
 	UCSR0B = _BV(RXEN0)|_BV(TXEN0);
 	UCSR0C = _BV(UCSZ01)|_BV(UCSZ00);
-#if 0 /* 1: 9600, 0: 115200 */
-	UBRR0 = 103; /* 9k6 bps @ 16 MHz (U2X0=0) */
-#else
+	// UBRR0 = 103; /* 9k6 bps @ 16 MHz (U2X0=0) */
+	// UCSR0A = 0;
+
 	UBRR0 = 16; /* 115k2 bps @ 16 MHz (U2X0=1) */
 	UCSR0A = _BV(U2X0);
-#endif
-       	UDR0 = '*'; /* say hello */
 
-	DDRB |= _BV(5);  /* turn on LED */
-	PORTB |= _BV(5);
-
-	DDRD |= _BV(5);
-	PORTD |= _BV(5);
+	for(i=0;i<sizeof(hello);i++)
+		serial_putchar(pgm_read_byte(&hello[i]));
 
 	avr_redeye_init();
 	sei();
 
 	while(1){
-		if(UCSR0A & _BV(RXC0)){
+		if(UCSR0A & _BV(RXC0)){ /* received a byte */
 			avr_redeye_put(UDR0);
-			serial_putchar('\r');
-			serial_putchar('\n');
-		}
-		if((UCSR0A & _BV(UDRE0))){
-			if(avr_redeye_data.state != 0){
-				serial_putchar('<');
-				serial_put_uint8_t(avr_redeye_data.state);
-				serial_putchar(':');
-				serial_put_uint16_t(avr_redeye_data.frame);
-				serial_putchar('>');
-			}
 		}
 	}
 }
